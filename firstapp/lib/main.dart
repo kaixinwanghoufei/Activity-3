fa185aeb0d88e4f5f8931d520ada05e1350636c2
kaixinwanghoufei <hongyu_fu@cmu.ac.th>
2025-12-16 15:48:26 +0700

    BottomNavigationBar

--- firstapp/lib/main.dart
+++ firstapp/lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:firstapp/pages/cal.dart';
+import 'package:firstapp/pages/contact.dart';
+import 'package:firstapp/pages/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,21 +10,24 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tui shop',
+      title: 'Pattama Shop',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          // 修复 dot-shorthands 为标准语法
+          seedColor: const Color.fromARGB(255, 218, 19, 191),
+        ),
       ),
-      home: const MyHomePage(title: 'Pattama Shop'),
-      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Calculate Change'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -30,21 +36,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var price = TextEditingController();
-  var amount = TextEditingController();
-  var change = TextEditingController();
-
-  double _total = 0;
-  double _change = 0;
-  String _changeMessage = '';
-
-  @override
-  void dispose() {
-    price.dispose();
-    amount.dispose();
-    change.dispose();
-    super.dispose();
-  }
+  int _currentIndex = 0;
+  final tabs = const [
+    // 添加 const 优化
+    HomePage(),
+    CalculatePage(),
+    ContactPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -53,135 +51,24 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Text(
-                "Change Calculation",
-                style: TextStyle(
-                  fontFamily: "maa",
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.blue,
-                  backgroundColor: Colors.red,
-                ),
-              ),
-              Image.asset("assets/1.jpg", height: 100),
-              Image.network(
-                'https://s3.ifanr.com/wp-content/uploads/2014/02/3dgifs6.gif',
-                height: 120,
-              ),
-              SizedBox(height: 8),
-              priceTextField(),
-              amountTextField(),
-              calculateButton(),
-              showTotalText(),
-              receiveMoneyTextField(),
-              changeCalculateButton(),
-              showChangeText(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget priceTextField() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        controller: price,
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'price per item',
-        ),
-        keyboardType: TextInputType.number,
-      ),
-    );
-  }
-
-  Widget amountTextField() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        controller: amount,
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'amount of items',
-        ),
-        keyboardType: TextInputType.number,
-      ),
-    );
-  }
-
-  Widget calculateButton() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ElevatedButton(
-        onPressed: () {
-          if (price.text.isNotEmpty && amount.text.isNotEmpty) {
-            double p = double.parse(price.text);
-            double a = double.parse(amount.text);
-
-            setState(() {
-              _total = p * a;
-            });
-          }
+      body: tabs[_currentIndex], // 修复格式
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: const [
+          // 添加 const
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calculate), label: "Calculation"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.contact_page), label: "Contact"),
+        ],
+        onTap: (index) {
+          setState(() {
+            // print(index);
+            _currentIndex = index;
+          });
         },
-        child: const Text('Calculate Total'),
       ),
     );
-  }
-
-  Widget showTotalText() {  : $_total Baht'),
-    );
-  }
-
-  Widget receiveMoneyTextField() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        controller: change,
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(),
-          labelText: 'get money',
-        ),
-        keyboardType: TextInputType.number,
-      ),
-    );
-  }
-
-  Widget changeCalculateButton() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ElevatedButton(
-        onPressed: () {
-          if (change.text.isNotEmpty) {
-            double money = double.parse(change.text);
-
-            setState(() {
-              if (money < _total) {
-                _change = 0;
-                _changeMessage = 'Money is not enough.';
-              } else {
-                _change = money - _total;
-                _changeMessage = '';
-              }
-            });
-          }
-        },
-        child: const Text('Calculate Change'),
-      ),
-    );
-  }
-
-  Widget showChangeText() {
-    final text =
-        _changeMessage.isNotEmpty ? _changeMessage : 'Change : $_change Baht';
-
-    return Padding(padding: const EdgeInsets.all(8.0), child: Text(text));
   }
 }
